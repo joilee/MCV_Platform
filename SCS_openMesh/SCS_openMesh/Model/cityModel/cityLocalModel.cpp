d34329d95be00ba6c20a3b3c458664b0f2cd4ae9
alisalh <508091602@qq.com>
2018-03-10 10:25:48 +0800

    上顶面剖分问题已修改
    0310

--- SCS_openMesh/SCS_openMesh/Model/cityModel/cityLocalModel.cpp
+++ SCS_openMesh/SCS_openMesh/Model/cityModel/cityLocalModel.cpp
@@ -210,7 +210,7 @@ void cityLocalModel::generateBuildingMesh()
 					N = VectorCross(E1, E2);
 					if (N.norm() > DOUBLE_EPSILON)
 					{
-						F.push_back(Vector3i(tmp->pre->id, tmp->id, tmp->next->id));
+						F.push_back(Vector3i(tmp->next->id, tmp->id, tmp->pre->id));
 						NF.push_back(N.normalize());
 					}
 					node* n = tmp->next;
@@ -232,7 +232,7 @@ void cityLocalModel::generateBuildingMesh()
 		N = VectorCross(E1, E2);
 		if (N.norm() > DOUBLE_EPSILON)
 		{
-			F.push_back(Vector3i(tmp->pre->id, tmp->id, tmp->next->id));
+			F.push_back(Vector3i(tmp->next->id, tmp->id, tmp->pre->id));
 			NF.push_back(N.normalize());
 		}
 	}
